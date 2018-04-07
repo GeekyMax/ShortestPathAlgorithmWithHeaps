dc489b194a4afe12db03ee69cbc98a703912ad10
GeekyMax <maxlovehmx@gmail.com>
2018-04-07 23:08:33 +0800

    换用邻接表，读大数据

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -4,6 +4,19 @@
 
 #include "dijkstra.h"
 
+void add_edge(Graph graph, int start, int end, Type length) {
+    adjacentNode node = new _adjacentNode();
+    node->end = end;
+    node->length = length;
+    node->next = graph->head[start];
+    graph->head[start] = node;
+    adjacentNode node1 = new _adjacentNode();
+    node1->end = start;
+    node1->length = length;
+    node1->next = graph->head[end];
+    graph->head[end] = node1;
+}
+
 /**
  * first line, the number of vex v
  * then, the number of edge e
@@ -15,18 +28,19 @@ Graph create_graph() {
 //    alloc the space gor graph
     Graph graph = (Graph) malloc(sizeof(struct _graph));
 //    init the matrix
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            graph->matrix[i][j] = INF;
-        }
+    for (int i = 0; i < MAX; ++i) {
+        graph->head[i] = nullptr;
     }
 //    read the numbers
+    char c;
     cin >> graph->vertex_num >> graph->edge_num;
 //    read each lines
     for (int i = 0; i < graph->edge_num; i++) {
-        cin >> length >> start >> end;
-        graph->matrix[start][end] = min(graph->matrix[start][end], length);
-        graph->matrix[end][start] = min(graph->matrix[end][start], length);
+        cin >> c >> start >> end >> length;
+        add_edge(graph, start, end, length);
+        if (i % 200 == 0) {
+            cout << i << endl;
+        }
     }
     return graph;
 }
@@ -39,6 +53,7 @@ Graph create_graph() {
  * @param dist an array to store the distance of each node
  */
 void dijkstra(Heap *heap, Graph G, int vs, int *prev, int *dist) {
+    int count = 0;
 //    clear the heap
     heap->clear();
 //    this array store the flag of whether the node have found the min distance
@@ -51,23 +66,30 @@ void dijkstra(Heap *heap, Graph G, int vs, int *prev, int *dist) {
 //    push first origin node to the priority queue
     heap->push(vs, 0);
 //    if the heap is not empty, pop one key and then update all adjacent node's distance.
+    adjacentNode temp_adjacent_node = nullptr;
     while (!heap->empty()) {
-        int u = heap->top().first;
+        if (count % 100 == 0) {
+            cout << count << endl;
+        }
+        count++;
+        int this_node = heap->top().first;
         int dis = heap->top().second;
         heap->pop();
-        if (flag[u]) {
+        if (flag[this_node]) {
             continue;
         }
-        dist[u] = dis;
-        flag[u] = true;
-        for (int i = 0; i < G->vertex_num; i++) {
-//            update the dist of this node's adjacent nodes
-//            if we get a smaller dist, we will insert it to the heap
-            if (G->matrix[u][i] + dis < dist[i]) {
-                dist[i] = G->matrix[u][i] + dis;
-                prev[i] = u;
-                heap->push(i, dist[i]);
+        dist[this_node] = dis;
+        flag[this_node] = true;
+        int index;
+        temp_adjacent_node = G->head[this_node];
+        while (temp_adjacent_node != nullptr) {
+            index = temp_adjacent_node->end;
+            if (temp_adjacent_node->length + dis < dist[index]) {
+                dist[index] = temp_adjacent_node->length + dis;
+                prev[index] = this_node;
+                heap->push(index, dist[index]);
             }
+            temp_adjacent_node = temp_adjacent_node->next;
         }
     }
 }
--- dijkstra.h
+++ dijkstra.h
@@ -9,12 +9,18 @@
 #include "Heap.h"
 
 using namespace std;
+typedef struct _adjacentNode {
+    int end;
+    Type length;
+    struct _adjacentNode* next;
+} *adjacentNode;
 
 typedef struct _graph {
     int vertex_num;           // 顶点数
     int edge_num;           // 边数
-    int matrix[MAX][MAX]; // 邻接矩阵
+    adjacentNode head[MAX]; // 邻接矩阵
 } *Graph;
+
 
 /**
  * first line, the number of vex v
