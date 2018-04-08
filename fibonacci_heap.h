b2958a5aa9e974c72bf2f80221abd92be5178949
GeekyMax <maxlovehmx@gmail.com>
2018-04-08 11:28:01 +0800

    more comments

--- fibonacci_heap.h
+++ fibonacci_heap.h
@@ -18,10 +18,12 @@ using namespace std;
         unsigned int _i = 0; \
         __asm__("bsr %1, %0" : "=r" (_i) : "r" ((x))); \
         _i; })
-#else   // 注意：通过gcc编译时，要添加 -lm 选项。
+#else
 #define LOG2(x) ((log((double)(x))) / (log(2.0)))
 #endif
-
+/**
+ * the structure of fibonacci node
+ */
 typedef struct _FibonacciNode {
     pair<int, Type> element;
     Type key;                        // 关键字(键值)
@@ -32,7 +34,12 @@ typedef struct _FibonacciNode {
     struct _FibonacciNode *parent;    // 父节点
     int marked;                       //是否被删除第1个孩子(1表示删除，0表示未删除)
 } FibonacciNode, FibNode;
-
+/**
+ * the structure of the fibonacci heap
+ * keyNum: the total number of nodes
+ * maxDegree the max of the node's degree
+ *
+ */
 typedef struct _FibonacciHeap {
     int keyNum;                    // 堆中节点的总数
     int maxDegree;                // 最大度
@@ -40,16 +47,47 @@ typedef struct _FibonacciHeap {
     struct _FibonacciNode **cons;    // 最大度的内存区域
 } *FibHeap;
 
+/**
+ * make a new fibonacci node valuing key
+ * @param key
+ * @return
+ */
 static FibNode *fib_node_make(Type key);
 
+/**
+ * make an empty fibonacci heap
+ * @return
+ */
 FibHeap fib_heap_make();
 
+/**
+ * insert a new node into a fibonacci heap
+ * @param heap
+ * @param node
+ */
 static void fib_heap_insert_node(FibHeap heap, FibNode *node);
 
+/**
+ * create a new node valuing index and key, then insert it into the heap
+ * @param heap
+ * @param index
+ * @param key
+ */
 void fib_heap_insert_key(FibHeap heap, int index, Type key);
 
+/**
+ * extract the min node from the heap then return it
+ * @param heap
+ * @return the min node's pointer
+ */
 void fib_heap_extract_min(FibHeap heap);
 
+/**
+ * union two fibonacci heaps.
+ * @param h1
+ * @param h2
+ * @return
+ */
 FibHeap fib_heap_union(FibHeap h1, FibHeap *h2);
 
 #endif //SHORTESTPATHALGORITHMWITHHEAPS_FIBONACCI_HEAP_H
